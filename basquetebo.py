606146bead6f569ee5a2abcc6ade4db96c737b8f
Gabrel Matias <galogico@gmail.com>
2021-10-23 17:20:24 -0300

    fiz os ex

--- basquetebo.py
+++ basquetebo.py
@@ -1,10 +1,7 @@
 distancia = int(input())
-
 if distancia <= 800:
     print (1)
-
 if distancia > 800 and distancia <= 1400:
     print (2)
-
 if distancia > 1400:
     print (3)
---
+++ nota.py
@@ -0,0 +1,6 @@
+A = int(input())
+M= int(input())
+final = M*2
+final = final - A
+print(final)
+
